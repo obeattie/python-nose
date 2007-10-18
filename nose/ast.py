6331747505a05206e6f73c1fcae21ed698309023
Kumar McMillan <kumar.mcmillan@gmail.com>
2007-10-18 04:43:00 +0000

    a proof of concept ast visitor

---
+++ nose/ast.py
@@ -0,0 +1,46 @@
+
+import compiler
+from compiler import visitor
+from pprint import pprint
+
+class ModuleVisitor(object):
+    class Inna:
+        def inna2():
+            pass
+    def __init__(self, filename):
+        self.filename = filename
+        self.classes = []
+        self.functions = []
+        
+    def default(self, node):
+        for child in node.getChildNodes():
+            # visit = ASTVisitor.dispatch
+            self.visit(child, node.lineage + [node])
+            
+    def visitModule(self, node, lineage=[]):
+        node.name = self.filename
+        node.lineage = lineage
+        # descend into classes and functions
+        self.default(node)
+        
+    def visitClass(self, node, lineage=[]):
+        node.lineage = lineage
+        self.classes.append(node)
+        # descend into subclasses and functions...
+        self.default(node) 
+        
+    def visitFunction(self, node, lineage=[]):
+        node.lineage = lineage
+        self.functions.append(node)
+
+if __name__ == '__main__':
+    ast = compiler.parseFile(__file__)
+    modnode = ModuleVisitor(__file__)
+    visitor.walk(ast, modnode)
+    def _name(n):
+        return n.name
+    pprint([(n.name, n.lineno, [_name(p) for p in n.lineage]) 
+            for n in modnode.classes])
+    pprint([(n.name, n.lineno, [_name(p) for p in n.lineage]) 
+            for n in modnode.functions])
+            
